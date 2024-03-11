44c94a17119c504ad86de8d4cdf2a229fc544a39
nsandoya <natsandoya@gmail.com>
2024-03-10 23:13:16 -0500

    Banner: implementado; pendiente: animación más pulida | Footer: eventos asignados; pendiente: animación

--- src/js/main.js
+++ src/js/main.js
@@ -8,7 +8,35 @@ const videoC = document.querySelector(".video-container")
 const videoContainer = document.querySelector(".video-container") */
 /* const c = document.querySelector(".imgContainer") */
 
+function init_banner(){
+    const banner = document.querySelector(".init_banner")
+    setTimeout(()=>{
+        //banner.classList.remove("init_banner")
+        banner.classList.add("init_banner_close")
+        //banner.innerHTML=""
+    }, 1000)
+    
+}
+init_banner()
 
+window.onscroll = function(ev) {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+        // El usuario ha llegado al final de la página, cambia el color de fondo del footer
+        document.querySelector("footer").classList.remove("footer-hide-bg");
+        document.querySelector(".email").classList.remove("footer-hide-text");
+        let rrss = document.querySelectorAll(".rrss");
+        rrss.forEach((item) =>{
+            item.classList.remove("footer-text-initial")
+        })
+    }else{
+        document.querySelector("footer").classList.add("footer-hide-bg");
+        document.querySelector(".email").classList.add("footer-hide-text");
+        let rrss = document.querySelectorAll(".rrss");
+        rrss.forEach((item) =>{
+            item.classList.add("footer-text-initial")
+        })
+    }
+};
 
 // Generar galerías de proyectos
 const narrative = new ProjectGallery(
