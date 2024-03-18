c6cd9716e47e67c99cc639d8e38df04bafb8f646
nsandoya <natsandoya@gmail.com>
2024-03-18 13:48:54 -0500

    Swiper: problemas resueltos | Pendiente: dilucidar implementación en modals

---
+++ src/js/swiper_cd.js
@@ -0,0 +1,15 @@
+/* import SwiperCore, { Navigation, Pagination } from './swiper-bundle.min.js';
+        SwiperCore.use([Navigation, Pagination]); */
+        var swiper = new Swiper(".mySwiper", {
+            slidesPerView: 1,
+            spaceBetween: 30,
+            loop: true,
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+        });
