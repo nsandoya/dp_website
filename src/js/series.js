ddc671f9caac262889d0d1b2b9c541c2464a21a5
nsandoya <natsandoya@gmail.com>
2024-03-17 19:28:33 -0500

    Series, galeria: se implementó la descripción de cada foto dentro del modal

--- src/js/series.js
+++ src/js/series.js
@@ -15,46 +15,62 @@ const fotosLista = [
     new Foto({
         id: "1",
         name: "junior",
+        year: 2024,
+        place: "Daule"
     }),
     new Foto({
         id: "2",
         name: "nuria",
+        year: 2023,
+        place: "Tena"
 
     }),
     new Foto({
         id: "3",
         name: "las_maravillas",
+        year: 2022,
+        place: "Riobamba"
 
     }),
     new Foto({
         id: "4",
         name: "nada_bello",
+        year: 2021,
+        place: "Cuenca"
 
     }),
     new Foto({
         id: "5",
         name: "por_un_momento",
+        year: 2021,
+        place: "Quito"
 
     }),
     new Foto({
         id: "6",
         name: "junior",
+        year: 2023,
+        place: "Sao Paulo"
 
     }),
     new Foto({
         id: "7",
         name: "nuria",
+        year: 2018,
+        place: "Guayaquil"
 
     }),
     new Foto({
         id: "8",
         name: "las_maravillas",
-
+        year: 2022,
+        place: "Chicago"
     }),
     new Foto({
         id: "9",
         name: "nada_bello",
-
+        year: 2020,
+        place: "Guayaquil"
     }),
     /* new Foto({
         id: "10",
@@ -89,18 +105,25 @@ openModal.forEach(item=>{
 
         modalDialog.innerHTML = `
             <button id="closeModal"  style="width: 100px; height: 50px;"><i class="bi bi-x-lg"></i></button>
-            <img id="img-modal" src=${url} alt="" style="width: 82%; max-width: 85%; justify-self: center; align-self: center; border-radius: 10px">
+            <div style="display:grid; grid-template-columns: 1fr; grid-template-rows: fit-content 1fr">
+
+            <img id="img-modal" src=${url} alt="" style="width: 80%; max-width: 85%; justify-self: center; align-self: end; border-radius: 10px">
             
+            <span id="pieDeFoto" class="text-white text-center text-uppercase mt-3">${item.getAttribute('data-info')}</span>
+            </div>
+
             <button class="prev"><i class="bi bi-caret-left-fill icon-size"></i></button>
             <button class="next"><i class="bi bi-caret-right-fill icon-size"></i></button>
-
         `
 
         const prev = document.querySelector(".prev")
         const next = document.querySelector(".next")
         const imgModal = document.querySelector("#img-modal")
+        const pieDeFoto = document.querySelector("#pieDeFoto")
 
         let itemID = item.id; 
+        let photoInfo = item.getAttribute('data-info')
+
         prev.addEventListener('click', ()=>{
             itemID = parseInt(itemID) > 1 ? parseInt(itemID) - 1 : galeria.fotos.length;
             console.log(itemID)
@@ -108,6 +131,10 @@ openModal.forEach(item=>{
             let fotoUsar = foto[0]
             console.log(fotoUsar.url)
             imgModal.src = fotoUsar.url
+
+            photoInfo = `${fotoUsar.place}, ${fotoUsar.year}`
+            imgModal.src = fotoUsar.url
+            pieDeFoto.innerHTML = photoInfo
             
         })
         next.addEventListener('click', ()=>{
@@ -117,6 +144,10 @@ openModal.forEach(item=>{
             let fotoUsar = foto[0]
             console.log(fotoUsar.url)
             imgModal.src = fotoUsar.url
+
+            photoInfo = `${fotoUsar.place}, ${fotoUsar.year}`
+            imgModal.src = fotoUsar.url
+            pieDeFoto.innerHTML = photoInfo
             
         })
         
