0e5062e266965ce49c1fc195c7487d3fedc83cdd
nsandoya <natsandoya@gmail.com>
2024-03-19 21:22:30 -0500

    Titulos y banner init: cambios para test, 3

--- src/js/main.js
+++ src/js/main.js
@@ -33,15 +33,15 @@ projectTitle.forEach(item => {
     // cambio de 'mouseover''mouseout' a 'touchstart''touchend'
     item.addEventListener('touchstart', ()=>{
         console.log("Cambio en el título (touch)")
-        item.querySelector(".project-title-gallery h2").classList.remove("no-display-title");
         item.querySelector(".project-title-gallery h2").classList.add("project-title-transition");
+        item.querySelector(".project-title-gallery h2").classList.remove("no-display-title");
         //item.style.transition = "all 0.3s cubic-bezier(.82,.42,.44,1) !important;"
     })
     item.addEventListener('touchend', ()=>{
         console.log("Cambio en el título (touch)")
 
         item.querySelector(".project-title-gallery h2").classList.add("no-display-title");
-        item.querySelector(".project-title-gallery h2").classList.remove("project-title-transition");
+        /* item.querySelector(".project-title-gallery h2").classList.remove("project-title-transition"); */
     })
 
     
